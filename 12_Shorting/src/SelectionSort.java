1c4a87c5f5eaec6ab6c9e886bd5edb9f55561674
bha8081 <bhaveshy2000r@gmail.com>
2024-04-20 13:40:05 +0530

    Selection Sort in Java

---
+++ 12_Shorting/src/SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SelectionSort {
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {43, 12, 27, 95, 48};
+
+        //selection sort
+        for(int i=0; i<arr.length-1; i++) {
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[j] < arr[smallest]) {
+                    smallest = j;
+                }
+            }
+            //swap
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+    }
+}

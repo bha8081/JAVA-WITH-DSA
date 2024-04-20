ce1b8fc8aae62bf7d7a27e73cb990eaf4d2bb01c
bha8081 <bhaveshy2000r@gmail.com>
2024-04-20 13:00:50 +0530

    Card Board code

---
+++ 03_Student/src/Student.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter your name: ");
+//        String studentName = sc.nextLine();
+//
+//        System.out.println("Student Name is ' "+studentName);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Bhavesh Yadav Love You!!");
+        }
+
+    }
+}
